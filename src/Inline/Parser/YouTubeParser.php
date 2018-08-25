5969b44737933aa910e5e285cb9b34aafa5a1323
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-08-25 10:40:27 +1000

    Removed duped docs.

--- src/Inline/Parser/YouTubeParser.php
+++ src/Inline/Parser/YouTubeParser.php
@@ -2,33 +2,15 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Alt Three Emoji.
- *
- * (c) Alt Three Services Limited
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace JohnnyHuy\Laravel\Inline\Parser;
 
 use JohnnyHuy\Laravel\Inline\Element\YouTube;
 use League\CommonMark\Inline\Parser\AbstractInlineParser;
 use League\CommonMark\InlineParserContext;
 
-/**
- * This is the emoji parser class.
- *
- * @author Graham Campbell <graham@alt-three.com>
- */
 class YouTubeParser extends AbstractInlineParser
 {
     /**
-     * Parse a line and determine if it contains an emoji.
-     *
-     * If it does, then we do the necessary.
-     *
      * @param InlineParserContext $inlineContext
      * @return bool
      */
