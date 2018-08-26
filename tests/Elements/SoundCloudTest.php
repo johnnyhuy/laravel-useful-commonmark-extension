7dedd4c7ad53efa588763ec8411f7f107377c189
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-08-26 18:58:43 +1000

    Added ext-json to Composer config.

---
+++ tests/Elements/SoundCloudTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JohnnyHuy\Laravel\Markdown\Tests\Elements;
+
+use JohnnyHuy\Laravel\Markdown\Tests\BaseTestCase;
+
+/**
+ * CommonMak markdown extension test
+ *
+ * @author Johnny Huynh <info@johnnyhuy.com>
+ */
+class YouTubeTest extends BaseTestCase
+{
+    public function successfulStrings()
+    {
+        $expected = '<p><span class="youtube-video"><iframe width="640" height="390" src="https://www.youtube.com/embed/52c_QSg64fs" type="text/html" frameborder="0"></iframe></span></p>';
+
+        return [
+            [':youtube http://www.youtube.com/watch?v=52c_QSg64fs', $expected],
+            [':youtube https://www.youtube.com/watch?v=52c_QSg64fs', $expected],
+            [':youtube http://youtube.com/watch?v=52c_QSg64fs', $expected],
+            [':youtube youtube.com/watch?v=52c_QSg64fs', $expected],
+            [':youtube http://www.youtube.com/watch?v=52c_QSg64fs?t=10s', $expected],
+            [':youtube http://www.youtube.com/watch?v=52c_QSg64fs?t=10s&something=123123123SDqweas', $expected],
+        ];
+    }
+
+    public function failedStrings()
+    {
+        return [
+            // YouTube keyword is not separated with a space
+            [':youtubehttps://www.youtube.com/watch?v=USL6P8haroY', '<p>:youtubehttps://www.youtube.com/watch?v=USL6P8haroY</p>'],
+
+            // Didn't include the ':youtube' keyword
+            ['https://www.youtube.com/watch?v=USL6P8haroY', '<p>https://www.youtube.com/watch?v=USL6P8haroY</p>'],
+
+            // Invalid YouTube URLs
+            [':youtube https//www.youtube.com/watch?v=USL6P8haroY', '<p>:youtube https//www.youtube.com/watch?v=USL6P8haroY</p>'],
+            [':youtube httpswww.youtube.com/watch?v=USL6P8haroY', '<p>:youtube httpswww.youtube.com/watch?v=USL6P8haroY</p>'],
+            [':youtube .youtube.com/watch?v=USL6P8haroY', '<p>:youtube .youtube.com/watch?v=USL6P8haroY</p>'],
+            [':youtube .com/watch?v=USL6P8haroY', '<p>:youtube .com/watch?v=USL6P8haroY</p>'],
+            [':youtube poop.com/watch?v=USL6P8haroY', '<p>:youtube poop.com/watch?v=USL6P8haroY</p>'],
+        ];
+    }
+
+    /**
+     * @dataProvider successfulStrings
+     * @param $input
+     * @param $output
+     */
+    public function testShouldRender($input, $output)
+    {
+        $this->assertSame("$output\n", $this->app->markdown->convertToHtml($input));
+    }
+
+    /**
+     * @dataProvider failedStrings
+     * @param $input
+     * @param $output
+     */
+    public function testShouldNotRender($input, $output)
+    {
+        $this->assertSame("$output\n", $this->app->markdown->convertToHtml($input));
+    }
+}
