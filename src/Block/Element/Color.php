e30f43f7eb78376e79701e54185af254fc26a4da
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-08-28 21:34:28 +1000

    Cleaned up licensing.

---
+++ src/Block/Element/Color.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace JohnnyHuy\Laravel\Block\Element;
+
+use League\CommonMark\Block\Element\AbstractBlock;
+use League\CommonMark\Block\Element\InlineContainerInterface;
+use League\CommonMark\ContextInterface;
+use League\CommonMark\Cursor;
+use League\CommonMark\Util\RegexHelper;
+
+class Color extends AbstractBlock implements InlineContainerInterface {
+    /**
+     * Returns true if this block can contain the given block as a child node
+     *
+     * @param AbstractBlock $block
+     *
+     * @return bool
+     */
+    public function canContain(AbstractBlock $block)
+    {
+        return true;
+    }
+
+    /**
+     * Returns true if block type can accept lines of text
+     *
+     * @return bool
+     */
+    public function acceptsLines()
+    {
+        return false;
+    }
+
+    /**
+     * Whether this is a code block
+     *
+     * @return bool
+     */
+    public function isCode()
+    {
+        return false;
+    }
+
+    /**
+     * Checks the next line, if true then continue.
+     *
+     * @param Cursor $cursor
+     * @return bool
+     */
+    public function matchesNextLine(Cursor $cursor)
+    {
+        return true;
+    }
+
+    /**
+     * @param ContextInterface $context
+     * @param Cursor           $cursor
+     */
+    public function handleRemainingContents(ContextInterface $context, Cursor $cursor)
+    {
+        $context->getTip()->addLine($cursor->getRemainder());
+    }
+
+    /**
+     * @param Cursor $cursor
+     * @param int    $currentLineNumber
+     *
+     * @return bool
+     */
+    public function shouldLastLineBeBlank(Cursor $cursor, $currentLineNumber)
+    {
+        return true;
+    }
+}
--- src/Block/Parser/TextAlignmentParser.php
+++ src/Block/Parser/TextAlignmentParser.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Alt Three Emoji.
- *
- * (c) Alt Three Services Limited
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace JohnnyHuy\Laravel\Block\Parser;
 
 use JohnnyHuy\Laravel\Block\Element\TextAlignment;
@@ -26,10 +17,6 @@ use League\CommonMark\Cursor;
 class TextAlignmentParser extends AbstractBlockParser
 {
     /**
-     * Parse a line and determine if it contains an emoji.
-     *
-     * If it does, then we do the necessary.
-     *
      * @param ContextInterface $context
      * @param Cursor $cursor
      * @return bool
