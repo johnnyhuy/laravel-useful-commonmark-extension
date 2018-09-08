5738cfc76d2504d0274b42ae0fb15c8c054999cf
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-09-08 12:13:00 +1000

    Cleaned up cloned licencing.

--- src/UsefulCommonMarkExtension.php
+++ src/UsefulCommonMarkExtension.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Alt Three Emoji.
- *
- * (c) Alt Three Services Limited
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace JohnnyHuy\Laravel;
 
 use Illuminate\Container\Container;
--- src/UsefulCommonMarkExtensionServiceProvider.php
+++ src/UsefulCommonMarkExtensionServiceProvider.php
@@ -2,19 +2,8 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Alt Three Emoji.
- *
- * (c) Alt Three Services Limited
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace JohnnyHuy\Laravel;
 
-use Illuminate\Contracts\Container\Container;
-use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
 use JohnnyHuy\Laravel\Block\Parser\TextAlignmentParser;
 use JohnnyHuy\Laravel\Inline\Parser\SoundCloudParser;
@@ -27,32 +16,6 @@ use JohnnyHuy\Laravel\Inline\Parser\YouTubeParser;
  */
 class UsefulCommonMarkExtensionServiceProvider extends ServiceProvider
 {
-    /**
-     * Boot the service provider.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->setupConfig();
-    }
-
-    /**
-     * Setup the config.
-     *
-     * @return void
-     */
-    protected function setupConfig()
-    {
-        $source = realpath($raw = __DIR__.'/../config/emoji.php') ?: $raw;
-
-        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
-            $this->publishes([$source => config_path('emoji.php')]);
-        }
-
-        $this->mergeConfigFrom($source, 'emoji');
-    }
-
     /**
      * Register the service provider.
      *
